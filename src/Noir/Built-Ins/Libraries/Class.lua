560a11e67e053cb33a49d29a5b30c0a6de61fc5c
Cuh4 <89425262+Cuh4@users.noreply.github.com>
2024-06-01 07:02:18 +0100

    better comments, add logging, and separate classobject from class intellisense-wise

--- src/Noir/Built-Ins/Libraries/Class.lua
+++ src/Noir/Built-Ins/Libraries/Class.lua
@@ -32,7 +32,9 @@
 -------------------------------
 
 --[[
-    A library that allows you to define classes which you can create objects from.
+    A library that allows you to define classes which you can create objects from.<br>
+    Note that classes can inherit from other classes.<br>
+    Adapted from [this repo.](https://github.com/Cuh4/LuaClasses)
 
     local MyClass = Class:Create("MyClass")
 
@@ -50,7 +52,8 @@
 Noir.Libraries.Class = Noir.Libraries:Create("NoirClass")
 
 --[[
-    Create a class that objects can be created from.
+    Create a class that objects can be created from.<br>
+    Note that classes can inherit from other classes.
 
     local MyClass = Class:Create("MyClass")
 
@@ -68,25 +71,27 @@ Noir.Libraries.Class = Noir.Libraries:Create("NoirClass")
 ---@param name string
 ---@param parent NoirClass|nil
 function Noir.Libraries.Class:Create(name, parent)
-    -- // Class Creation
     -- Create a class
     ---@type NoirClass
     local class = {} ---@diagnostic disable-line
     class.__name = name
     class.__parent = parent
+    class.isObject = false
 
     function class:New(...)
-        -- create object
+        -- create Object
         ---@type NoirClassObject
-        local object = {}
+        local object = {} ---@diagnostic disable-line
         self:__descend(object, {New = true, Init = true, __descend = true})
 
-        -- call init of object. this init function will provide the needed attributes to the object
+        object.isObject = true
+
+        -- Call init of object. This init function will provide the needed attributes to the object
         if self.Init then
             self.Init(object, ...)
         end
 
-        -- return the object
+        -- Return the object
         return object
     end
 
@@ -107,16 +112,27 @@ function Noir.Libraries.Class:Create(name, parent)
     end
 
     function class:InitializeParent(...)
-        if not self.__parent then
+        -- Check if this was called from an object
+        if not self.isObject then
+            Noir.Libraries.Logging:Error(self.__name, "Attempted to call :InitializeParent() when 'self' is a class and not an object.")
             return
         end
 
+        -- Check if there is a parent
+        if not self.__parent then
+            Noir.Libraries.Logging:Error(self.__name, "Attempted to call :InitializeParent() when 'self' has no parent.")
+            return
+        end
+
+        -- Create an object from the parent class
         local object = self.__parent:New(...)
-        self.__descend(object, self, {})
+
+        -- Copy and bring new attributes and methods down from the new parent object to this object
+        self.__descend(object, self, {New = true, Init = true, __descend = true})
     end
 
-    function class:IsSameType(object)
-        return object.__name and self.__name == object.__name
+    function class:IsSameType(other)
+        return other.__name ~= nil and self.__name == other.__name
     end
 
     return class
@@ -129,11 +145,16 @@ end
 ---@class NoirClass
 ---@field __name string The name of this class
 ---@field __parent NoirClass|nil The parent class that this class inherits from
+---@field isObject false
 ---@field Init fun(self: NoirClassObject, ...) A function that initializes objects created from this class
 ---
 ---@field New fun(self: NoirClass, ...: any): NoirClassObject A method to create an object from this class
 ---@field __descend fun(from: NoirClass|NoirClassObject, object: NoirClassObject, exceptions: table<any, boolean>) A helper function that copies important values from the class to an object
+---@field IsSameType fun(self: NoirClass, other: NoirClass|NoirClassObject): boolean A method that returns whether an object is identical to this one
+
+---@class NoirClassObject An object created from a class
+---@field isObject true
+---@field __name string The name of the class that created this object
+---@field __parent NoirClass|nil The parent class that this object inherits from
 ---@field InitializeParent fun(self: NoirClassObject, ...: any) A method that initializes the parent class for this object
 ---@field IsSameType fun(self: NoirClassObject, object: NoirClassObject): boolean A method that returns whether an object is identical to this one
-
----@class NoirClassObject: NoirClass An object created from a class
