63dede6ef3659e5073e8850a5b42c464aece56c5
Cuh4 <89425262+Cuh4@users.noreply.github.com>
2024-06-01 02:13:56 +0100

    more documentation

--- src/Noir/Libraries.lua
+++ src/Noir/Libraries.lua
@@ -30,12 +30,22 @@
 -------------------------------
 -- // Main
 -------------------------------
+
+--[[
+    A module of Noir that allows you to create your own libraries to use throughout your code.
+
+    MyLibrary = Noir.Libraries:Create("MyLibrary")
+
+    function MyLibrary:add(num1, num2)
+        return num1 + num2
+    end
+]]
 Noir.Libraries = {}
 
 --[[
     Returns a library starter pack for you to use when creating libraries for your addon.
 
-    local MyLibrary = Noir.Libraries:Create("MyLibrary")
+    MyLibrary = Noir.Libraries:Create("MyLibrary")
 
     function MyLibrary:add(num1, num2)
         return num1 + num2
