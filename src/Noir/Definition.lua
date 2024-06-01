2aa95df75d7d4eaf59e7428e0856568ff0d252ef
Cuh4 <89425262+Cuh4@users.noreply.github.com>
2024-06-01 02:14:27 +0100

    place into separate file

---
+++ src/Noir/Definition.lua
@@ -0,0 +1,38 @@
+--------------------------------------------------------
+-- [Noir] Definition
+--------------------------------------------------------
+
+--[[
+    ----------------------------
+
+    CREDIT:
+        Author: @Cuh4 (GitHub)
+        GitHub Repository: https://github.com/cuhHub/NoirFramework
+
+    License:
+        Copyright (C) 2024 Cuh4
+
+        Licensed under the Apache License, Version 2.0 (the "License");
+        you may not use this file except in compliance with the License.
+        You may obtain a copy of the License at
+
+            http://www.apache.org/licenses/LICENSE-2.0
+
+        Unless required by applicable law or agreed to in writing, software
+        distributed under the License is distributed on an "AS IS" BASIS,
+        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        See the License for the specific language governing permissions and
+        limitations under the License.
+
+    ----------------------------
+]]
+
+-------------------------------
+-- // Main
+-------------------------------
+
+--[[
+    Noir Framework @ https://github.com/cuhHub/NoirFramework<br>
+    A framework for making Stormworks addons with ease.
+]]
+Noir = {}
