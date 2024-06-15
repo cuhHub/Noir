c68daa161f34a1f96ab96306fc5e4ff626487ec9
Cuh4 <89425262+Cuh4@users.noreply.github.com>
2024-06-15 20:12:56 +0100

    add :GetPermissions() method to player class

--- src/Noir/Noir.lua
+++ src/Noir/Noir.lua
@@ -35,7 +35,7 @@
     The current version of Noir.<br>
     Follows [Semantic Versioning.](https://semver.org)
 ]]
-Noir.Version = "1.5.1"
+Noir.Version = "1.6.1"
 
 --[[
     This event is called when the framework is started.<br>
