d2612539c4590568ca2dafefe151b30520146ebe
Cuh4 <89425262+Cuh4@users.noreply.github.com>
2024-06-16 19:24:36 +0100

    change version

--- src/Noir/Noir.lua
+++ src/Noir/Noir.lua
@@ -35,7 +35,7 @@
     The current version of Noir.<br>
     Follows [Semantic Versioning.](https://semver.org)
 ]]
-Noir.Version = "1.6.2"
+Noir.Version = "1.7.0"
 
 --[[
     This event is called when the framework is started.<br>
