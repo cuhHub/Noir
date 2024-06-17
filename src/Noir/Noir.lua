c44862450c07afa7d4fcf083ff894ea7a2c01da9
Cuh4 <89425262+Cuh4@users.noreply.github.com>
2024-06-17 17:56:44 +0100

    update version

--- src/Noir/Noir.lua
+++ src/Noir/Noir.lua
@@ -35,7 +35,7 @@
     The current version of Noir.<br>
     Follows [Semantic Versioning.](https://semver.org)
 ]]
-Noir.Version = "1.8.3"
+Noir.Version = "1.8.4"
 
 --[[
     This event is called when the framework is started.<br>
