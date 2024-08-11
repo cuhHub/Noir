7f4d0a5127048d2f48ab0bc78308ab5f4fcbed48
Cuh4 <89425262+Cuh4@users.noreply.github.com>
2024-08-11 01:23:51 +0100

    fix(build): fix build.bat to use new combine tool location

--- build.bat
+++ build.bat
@@ -1,3 +1,1 @@
-cd tools
-py combine.py --directory "../src/Noir" --destination "../_build/Noir.lua" --allow_file_extension ".lua"
-cd ..
+py tools/combine/combine.py --directory "src/Noir" --destination "_build/Noir.lua" --allow_file_extension ".lua"
