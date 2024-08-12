6f5bd95a75daa6a50b1c8aff303a3ca3006484e0
Cuh4 <89425262+Cuh4@users.noreply.github.com>
2024-08-12 05:44:17 +0100

    feat(ChatService): add new message service for handling messages. will add cross-addon message synchronization in the future when i make an addon communication service

---
+++ src/Noir/Built-Ins/Classes/Message.lua
@@ -0,0 +1,124 @@
+--------------------------------------------------------
+-- [Noir] Classes - Message
+--------------------------------------------------------
+
+--[[
+    ----------------------------
+
+    CREDIT:
+        Author(s): @Cuh4 (GitHub)
+        GitHub Repository: https://github.com/cuhHub/Noir
+
+    License:
+        Copyright (C) 2024 Cuh4
+
+        Licensed under the Apache License, Version 2.0 (the "License");
+        you may not use this file except in compliance with the License.
+        You may obtain a copy of the License at
+
+            http://www.apache.org/licenses/LICENSE-2.0
+
+        Unless required by applicable law or agreed to in writing, software
+        distributed under the License is distributed on an "AS IS" BASIS,
+        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        See the License for the specific language governing permissions and
+        limitations under the License.
+
+    ----------------------------
+]]
+
+-------------------------------
+-- // Main
+-------------------------------
+
+--[[
+    Represents a message.
+]]
+---@class NoirMessage: NoirClass
+---@field New fun(self: NoirMessage, author: NoirPlayer|nil, isAddon: boolean, content: string, title: string, sentAt: number|nil, recipient: NoirPlayer|nil): NoirMessage
+---@field Author NoirPlayer|nil
+---@field IsAddon boolean
+---@field Content string
+---@field Title string If this message wasn't sent by an addon, this will be the author's name
+---@field SentAt number
+---@field Recipient NoirPlayer|nil nil = everyone
+Noir.Classes.MessageClass = Noir.Class("NoirMessage")
+
+--[[
+    Initializes message class objects.
+]]
+---@param author NoirPlayer|nil
+---@param isAddon boolean
+---@param content string
+---@param title string
+---@param sentAt number|nil
+---@param recipient NoirPlayer|nil
+function Noir.Classes.MessageClass:Init(author, isAddon, content, title, sentAt, recipient)
+    Noir.TypeChecking:Assert("Noir.Classes.MessageClass:Init()", "author", author, Noir.Classes.PlayerClass, "nil")
+    Noir.TypeChecking:Assert("Noir.Classes.MessageClass:Init()", "isAddon", isAddon, "boolean")
+    Noir.TypeChecking:Assert("Noir.Classes.MessageClass:Init()", "content", content, "string")
+    Noir.TypeChecking:Assert("Noir.Classes.MessageClass:Init()", "title", title, "string")
+    Noir.TypeChecking:Assert("Noir.Classes.MessageClass:Init()", "sentAt", sentAt, "number", "nil")
+    Noir.TypeChecking:Assert("Noir.Classes.MessageClass:Init()", "recipient", recipient, Noir.Classes.PlayerClass, "nil")
+
+    self.Author = author
+    self.IsAddon = isAddon
+    self.Content = content
+    self.Title = title
+    self.SentAt = sentAt or server.getTimeMillisec()
+    self.Recipient = recipient
+end
+
+--[[
+    Serializes the message into a g_savedata-safe format.<br>
+    Used internally.
+]]
+---@return NoirSerializedMessage
+function Noir.Classes.MessageClass:_Serialize()
+    return {
+        Author = self.Author and self.Author.ID or nil,
+        IsAddon = self.IsAddon,
+        Content = self.Content,
+        Title = self.Title,
+        SentAt = self.SentAt,
+        Recipient = self.Recipient
+    }
+end
+
+--[[
+    Deserializes the message from a g_savedata-safe format.<br>
+    Used internally.
+]]
+---@param serializedMessage NoirSerializedMessage
+function Noir.Classes.MessageClass:_Deserialize(serializedMessage)
+    -- Type checking
+    Noir.TypeChecking:Assert("Noir.Classes.MessageClass:_Deserialize()", "serializedMessage", serializedMessage, "table")
+
+    -- Create the message
+    local message = self:New(
+        serializedMessage.Author and Noir.Services.PlayerService:GetPlayer(serializedMessage.Author),
+        serializedMessage.IsAddon,
+        serializedMessage.Content,
+        serializedMessage.Title,
+        serializedMessage.SentAt,
+        serializedMessage.Recipient and Noir.Services.PlayerService:GetPlayer(serializedMessage.Recipient)
+    )
+
+    -- Return the message
+    return message
+end
+
+-------------------------------
+-- // Intellisense
+-------------------------------
+
+--[[
+    Represents a serialized version of the NoirMessage class.
+]]
+---@class NoirSerializedMessage
+---@field Author integer|nil
+---@field IsAddon boolean
+---@field Content string
+---@field Title string
+---@field SentAt number
+---@field Recipient integer|nil
---
+++ src/Noir/Built-Ins/Services/ChatService.lua
@@ -0,0 +1,273 @@
+--------------------------------------------------------
+-- [Noir] Services - Message Service
+--------------------------------------------------------
+
+--[[
+    ----------------------------
+
+    CREDIT:
+        Author(s): @Cuh4 (GitHub)
+        GitHub Repository: https://github.com/cuhHub/Noir
+
+    License:
+        Copyright (C) 2024 Cuh4
+
+        Licensed under the Apache License, Version 2.0 (the "License");
+        you may not use this file except in compliance with the License.
+        You may obtain a copy of the License at
+
+            http://www.apache.org/licenses/LICENSE-2.0
+
+        Unless required by applicable law or agreed to in writing, software
+        distributed under the License is distributed on an "AS IS" BASIS,
+        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        See the License for the specific language governing permissions and
+        limitations under the License.
+
+    ----------------------------
+]]
+
+-------------------------------
+-- // Main
+-------------------------------
+
+--[[
+    A service for storing, accessing and sending messages.
+
+    ---@param message NoirMessage
+    Noir.Services.MessageService.OnMessage:Connect(function(message)
+        Noir.Libraries.Logging:Info("Message", "(%s) > %s (%s)", message.Title, message.Content, message.IsAddon and "Sent by addon" or "Sent by player")
+    end)
+
+    Noir.Services.MessageService:SendMessage(nil, "[Server]", "Hello world!")
+]]
+---@class NoirMessageService: NoirService
+---@field Messages table<integer, NoirMessage> A table of all messages that have been sent.
+---@field _SavedMessages table<integer, NoirSerializedMessage> A table of all messages that have been sent (g_savedata version).
+---@field _MessageLimit integer The maximum amount of messages that can be stored.
+---
+---@field OnMessage NoirEvent Arguments: NoirMessage | Fired when a message is sent.
+---
+---@field _OnChatMessageConnection NoirConnection The connection to the `onChatMessage` event.
+Noir.Services.MessageService = Noir.Services:CreateService(
+    "MessageService",
+    true,
+    "A service for storing, accessing and sending messages.",
+    nil,
+    {"Cuh4"}
+)
+
+function Noir.Services.MessageService:ServiceInit()
+    self.Messages = {}
+    self._SavedMessages = self:Load("Messages", {})
+
+    self.OnMessage = Noir.Libraries.Events:Create()
+
+    self._MessageLimit = 220
+end
+
+function Noir.Services.MessageService:ServiceStart()
+    self:_LoadSavedMessages()
+
+    self._OnChatMessageConnection = Noir.Callbacks:Connect("onChatMessage", function(peerID, title, message)
+        -- Get author of message (player)
+        local author = Noir.Services.PlayerService:GetPlayer(peerID)
+
+        if not author then
+            Noir.Libraries.Logging:Error("MessageService", "`onChatMessage` gave an unrecognized peer ID.", false)
+            return
+        end
+
+        -- Register message
+        self:_RegisterMessage(
+            title,
+            message,
+            author,
+            false,
+            nil,
+            nil,
+            true
+        )
+    end)
+end
+
+--[[
+    Load all saved messages.<br>
+    Used internally.
+]]
+function Noir.Services.MessageService:_LoadSavedMessages()
+    -- Get messages
+    local messages = Noir.Libraries.Table:Copy(self._SavedMessages)
+
+    -- Ensure correct order
+    table.sort(messages, function(a, b)
+        return a.SentAt < b.SentAt
+    end)
+
+    -- Clear saved messages
+    self._SavedMessages = {}
+    self:Save("Messages", self._SavedMessages)
+
+    -- Register saved messages
+    for _, message in pairs(messages) do
+        self:_RegisterMessage(
+            message.Title,
+            message.Content,
+            message.Author and Noir.Services.PlayerService:GetPlayer(message.Author),
+            message.IsAddon,
+            message.SentAt,
+            message.Recipient and Noir.Services.PlayerService:GetPlayer(message.Recipient),
+            false
+        )
+    end
+end
+
+--[[
+    Insert a value into a table, removing the first value if the table is full.<br>
+    Used internally.
+]]
+---@param tbl table
+---@param value any
+---@param limit integer
+function Noir.Services.MessageService:_InsertIntoTable(tbl, value, limit)
+    -- Type checking
+    Noir.TypeChecking:Assert("Noir.Services.MessageService:_InsertIntoTable()", "tbl", tbl, "table")
+    Noir.TypeChecking:Assert("Noir.Services.MessageService:_InsertIntoTable()", "limit", limit, "number")
+
+    -- Insert
+    table.insert(tbl, value)
+
+    -- Remove
+    if #tbl > limit then
+        table.remove(tbl, 1)
+    end
+end
+
+--[[
+    Register a message.<br>
+    Used internally.
+]]
+---@param title string
+---@param content string
+---@param author NoirPlayer|nil
+---@param isAddon boolean
+---@param sentAt number|nil
+---@param recipient NoirPlayer|nil
+---@param fireEvent boolean|nil
+---@return NoirMessage
+function Noir.Services.MessageService:_RegisterMessage(title, content, author, isAddon, sentAt, recipient, fireEvent)
+    -- Type checking
+    Noir.TypeChecking:Assert("Noir.Services.MessageService:_RegisterMessage()", "title", title, "string")
+    Noir.TypeChecking:Assert("Noir.Services.MessageService:_RegisterMessage()", "content", content, "string")
+    Noir.TypeChecking:Assert("Noir.Services.MessageService:_RegisterMessage()", "author", author, Noir.Classes.PlayerClass, "nil")
+    Noir.TypeChecking:Assert("Noir.Services.MessageService:_RegisterMessage()", "isAddon", isAddon, "boolean")
+    Noir.TypeChecking:Assert("Noir.Services.MessageService:_RegisterMessage()", "sentAt", sentAt, "number", "nil")
+    Noir.TypeChecking:Assert("Noir.Services.MessageService:_RegisterMessage()", "recipient", recipient, Noir.Classes.PlayerClass, "nil")
+    Noir.TypeChecking:Assert("Noir.Services.MessageService:_RegisterMessage()", "fireEvent", fireEvent, "boolean", "nil")
+
+    -- Create message
+    local message = Noir.Classes.MessageClass:New(
+        author,
+        isAddon,
+        content,
+        title,
+        sentAt or server.getTimeMillisec(),
+        recipient
+    )
+
+    -- Register
+    self:_InsertIntoTable(self.Messages, message, self._MessageLimit)
+
+    -- Save
+    self:_SaveMessage(message)
+
+    -- Fire event
+    if fireEvent then
+        self.OnMessage:Fire(message)
+    end
+
+    -- Return
+    return message
+end
+
+--[[
+    Save a message.<br>
+    Used internally.
+]]
+---@param message NoirMessage
+function Noir.Services.MessageService:_SaveMessage(message)
+    -- Type checking
+    Noir.TypeChecking:Assert("Noir.Services.MessageService:_SaveMessage()", "message", message, Noir.Classes.MessageClass)
+
+    -- Save
+    self:_InsertIntoTable(self._SavedMessages, message:_Serialize(), self._MessageLimit)
+    self:Save("Messages", self._SavedMessages)
+end
+
+--[[
+    Send a message to a player or all players.
+
+    Noir.Services.MessageService:SendMessage(nil, "[Server]", "Hello, %s!", "world") -- "[Server] Hello, world!"
+]]
+---@param player NoirPlayer|nil nil = everyone
+---@param title string
+---@param content string
+---@param ... any
+---@return NoirMessage
+function Noir.Services.MessageService:SendMessage(player, title, content, ...)
+    -- Type checking
+    Noir.TypeChecking:Assert("Noir.Services.MessageService:SendMessage()", "player", player, Noir.Classes.PlayerClass, "nil")
+    Noir.TypeChecking:Assert("Noir.Services.MessageService:SendMessage()", "title", title, "string")
+    Noir.TypeChecking:Assert("Noir.Services.MessageService:SendMessage()", "content", content, "string")
+
+    -- Send message
+    local formattedContent = ... and content:format(...) or content
+    server.announce(title, formattedContent, player and player.ID)
+
+    -- Register message
+    local message = self:_RegisterMessage(
+        title,
+        formattedContent,
+        nil,
+        true,
+        nil,
+        player,
+        true
+    )
+
+    -- Return message
+    return message
+end
+
+--[[
+    Returns all messages sent by a player.
+]]
+---@param player NoirPlayer
+---@return table<integer, NoirMessage>
+function Noir.Services.MessageService:GetMessagesByPlayer(player)
+    -- Type checking
+    Noir.TypeChecking:Assert("Noir.Services.MessageService:GetMessagesByPlayer()", "player", player, Noir.Classes.PlayerClass)
+
+    -- Get messages
+    local messages = {}
+
+    for _, message in pairs(self:GetAllMessages()) do
+        if message.Author and Noir.Services.PlayerService:IsSamePlayer(player, message.Author) then
+            table.insert(messages, message)
+        end
+    end
+
+    -- Return
+    return messages
+end
+
+--[[
+    Returns all messages.<br>
+    Earliest entries in table = Oldest messages
+]]
+---@param copy boolean|nil Whether or not to copy the table (recommended), but may be slow
+---@return table<integer, NoirMessage>
+function Noir.Services.MessageService:GetAllMessages(copy)
+    Noir.TypeChecking:Assert("Noir.Services.MessageService:GetAllMessages()", "copy", copy, "boolean", "nil")
+    return copy and Noir.Libraries.Table:Copy(self.Messages) or self.Messages
+end
