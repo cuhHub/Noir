e4fc159e12abefa1c90524cd09b0c5a9a0e09088
Cuh4 <89425262+Cuh4@users.noreply.github.com>
2024-10-21 10:22:23 +0100

    revert(ef9d447): re-add improved TickIterationProcess

--- src/Noir/Built-Ins/Classes/TickIterationProcess.lua
+++ src/Noir/Built-Ins/Classes/TickIterationProcess.lua
@@ -35,22 +35,21 @@
     Represents a process in which code iterates through a table in chunks of x over how ever many necessary ticks.
 ]]
 ---@class NoirTickIterationProcess: NoirClass
----@field New fun(self: NoirTickIterationProcess, ID: number, tbl: table<integer, table<integer, any>>, chunkSize: integer): NoirTickIterationProcess
+---@field New fun(self: NoirTickIterationProcess, ID: number, tbl: table, chunkSize: integer): NoirTickIterationProcess
 ---@field ID integer The ID of this process
 ---@field IterationEvent NoirEvent Arguments: value (any), tick (integer), completed (boolean) | Fired when an iteration during a tick is occuring
 ---@field ChunkSize integer The number of values to iterate through per tick
 ---@field TableToIterate table The table to iterate through across ticks
----@field TableSize integer The number of values in the table
 ---@field CurrentTick integer Represents the current tick the iteration is at
 ---@field Completed boolean Whether or not the iteration is completed
----@field Chunks table<integer, table<integer, any>> The chunks of the table
+---@field _PreviousIndex any The last index used in iteration
 Noir.Classes.TickIterationProcessClass = Noir.Class("NoirTickIterationProcess")
 
 --[[
     Initializes tick iteration process class objects.
 ]]
 ---@param ID integer
----@param tbl table<integer, table<integer, any>>
+---@param tbl table
 ---@param chunkSize integer
 function Noir.Classes.TickIterationProcessClass:Init(ID, tbl, chunkSize)
     Noir.TypeChecking:Assert("Noir.Classes.TickIterationProcessClass:Init()", "ID", ID, "number")
@@ -61,11 +60,9 @@ function Noir.Classes.TickIterationProcessClass:Init(ID, tbl, chunkSize)
     self.IterationEvent = Noir.Libraries.Events:Create()
     self.ChunkSize = chunkSize
     self.TableToIterate = tbl
-    self.TableSize = #self.TableToIterate
     self.CurrentTick = 0
     self.Completed = false
-
-    self.Chunks = self:CalculateChunks()
+    self._PreviousIndex = nil
 end
 
 --[[
@@ -76,40 +73,32 @@ function Noir.Classes.TickIterationProcessClass:Iterate()
     -- Increment the current tick
     self.CurrentTick = self.CurrentTick + 1
 
-    -- Check if this is the final iteration
-    if not self.Chunks[self.CurrentTick] then
-        Noir.Libraries.Logging:Warning("NoirTickIterationProcess", ":Iterate() was called when the iteration process has already reached the end of the table")
-        return true
-    end
+    -- Iterate 
+    for _ = 1, self.ChunkSize do
+        -- Get next index and value
+        local index, value = next(self.TableToIterate, self._PreviousIndex)
 
-    -- Check if this is the final iteration
-    local chunk = self.Chunks[self.CurrentTick]
+        -- Nil check. This should never evaluate to true
+        if index == nil then
+            break
+        end
 
-    for index, value in pairs(chunk) do
         -- Set completed
-        local completed = self.CurrentTick >= #self.Chunks and index >= #chunk
+        local completed = (next(self.TableToIterate, index) == nil)
         self.Completed = completed
 
+        -- Set index
+        self._PreviousIndex = index
+
         -- Fire event
-        self.IterationEvent:Fire(value, self.CurrentTick, completed)
+        self.IterationEvent:Fire(index, value, self.CurrentTick, completed)
+
+        -- Break if completed
+        if completed then
+            break
+        end
     end
 
     -- Return
     return self.Completed
-end
-
---[[
-    Calculate the chunks of the table.
-]]
----@return table<integer, table<integer, any>>
-function Noir.Classes.TickIterationProcessClass:CalculateChunks()
-    -- Calculate chunks
-    local chunks = {}
-
-    for index = 1, self.TableSize, self.ChunkSize do
-        table.insert(chunks, Noir.Libraries.Table:Slice(self.TableToIterate, index, index + self.ChunkSize - 1))
-    end
-
-    -- Return
-    return chunks
 end
